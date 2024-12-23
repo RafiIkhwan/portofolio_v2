31fc11be330bac78bd9b5514c4f37ce2349e6499
RafiIkhwan <rafiikhwan2006@gmail.com>
2024-12-23 10:41:48 +0700

    update project - temansehat

--- src/utils/data.js
+++ src/utils/data.js
@@ -31,6 +31,11 @@ import LegalPlus_5 from './img/legalplus_5.png'
 import LegalPlus_6 from './img/legalplus_6.png'
 import LegalPlus_7 from './img/legalplus_7.png'
 
+import TemanSehat from './img/TemanSehat.png'
+import TemanSehat_1 from './img/TemanSehat_1.png'
+import TemanSehat_2 from './img/TemanSehat_2.png'
+import TemanSehat_3 from './img/TemanSehat_3.png'
+
 import PerpustakaanLaravel from './img/PerpustakaanLaravel.png'
 import PerpustakaanLaravel_1 from './img/PerpustakaanLaravel_1.png'
 import PerpustakaanLaravel_2 from './img/PerpustakaanLaravel_2.png'
@@ -43,6 +48,20 @@ import FortekVite from './img/fortekVite.png'
 
 export const projectData = [
     {
+      imageUrl: [TemanSehat, TemanSehat_1, TemanSehat_2, TemanSehat_3],
+      projectTitle: 'Teman Sehat',
+      projectDescription: 'Teman Sehat is a freelance project and innovative e-commerce platform offering quality health products, including medicines, supplements, and medical devices. Partnering with trusted pharmacies and manufacturers, Teman Sehat ensures safe, effective, and high-quality products at competitive prices, delivered right to your doorstep.',
+      projectUrl: '',
+      githubUrl: '',
+      builtWith: {
+        programmingLanguage: ['PHP', 'HTML', 'CSS'],
+        framework: ['Bootstrap'],
+        database: ['MySQL'],
+        toolsAndOther: ['Visual Studio Code'],
+      },
+      date: '2024-12-20',
+    },
+    {
       imageUrl: [LegalPlus_1, LegalPlus_2, LegalPlus_3, LegalPlus_4, LegalPlus_5, LegalPlus_6, LegalPlus_7],
       projectTitle: 'Legal Plus',
       projectDescription: 'Cloud-based law practice management software designed specifically for law firms in Indonesia to help lawyers run their practices more efficiently',
@@ -54,7 +73,7 @@ export const projectData = [
         database: ['MongoDB'],
         toolsAndOther: ['Visual Studio Code', 'NodeJs', 'Bun', 'Git'],
       },
-      date: '2024-02-20',
+      date: '2024-07-30',
     },
     {
       imageUrl: [BekangAD_2, BekangAD_3, BekangAD_4, BekangAD_5],
@@ -68,7 +87,7 @@ export const projectData = [
         database: ['PostgreSQL'],
         toolsAndOther: ['Visual Studio Code', 'Git'],
       },
-      date: '2024-02-20',
+      date: '2024-05-28',
     },
     {
       imageUrl: [Suxz_1, Suxz_2, Suxz_3, Suxz_4, Suxz_5, Suxz_6, Suxz_7],
